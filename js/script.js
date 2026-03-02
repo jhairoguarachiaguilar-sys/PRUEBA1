3930780067d11a0f4b48a3a4165b3a4c08ef84a2
JHAIRO LIONEL GUARACHI AGUILAR <jhairo.guarachiaguilar@intjem.edu.bo>
2026-03-02 09:53:40 -0400

    la primera v1 de mysitioPro

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Sitio cargado correctamente");
+
+
+    const enlaces = document.querySelectorAll("nav a");
+
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("mouseover", () => {
+            enlace.style.opacity = "0.8";
+        });
+
+
+        enlace.addEventListener("mouseout", () => {
+            enlace.style.opacity = "1";
+        });
+    });
+});
