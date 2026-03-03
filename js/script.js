1b0eba0981b6cdb58844e11dea74e1212f3bc3fd
Jhairo Lionel Guarachi Aguilar <jhairo.guarachiaguilar@intjem.edu.bo>
2026-03-02 20:40:59 -0400

    Actualizacion 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Sitio cargado correctamente");
 
@@ -17,23 +17,3 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Sitio cargado correctamente");
-
-
-    const enlaces = document.querySelectorAll("nav a");
-
-
-    enlaces.forEach(enlace => {
-        enlace.addEventListener("mouseover", () => {
-            enlace.style.opacity = "0.8";
-        });
-
-
-        enlace.addEventListener("mouseout", () => {
-            enlace.style.opacity = "1";
-        });
-    });
-});
->>>>>>> e1ceceacb33049ca69dac9fb8fac4cbdc37e0f4f
